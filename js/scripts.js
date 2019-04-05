c47a8531d6223bc7c8cbe845f3b552c8e1573634
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-04-05 21:58:04 +0300

    created another variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // var sum=[]
   // function denilson(){
   //   var dice1 = parseInt(Math.floor(Math.random()*6 +1))
@@ -7,12 +7,24 @@ $(document).ready(function(){
   //   alert(sum)
   // }
   //
+  var total1 = 0;
 
-$("#roll1").click(()=>{
-  event.preventDefault();
-    var dice1 = parseInt(Math.floor(Math.random()*6 +1))
-  $(".den").text(dice1)
+  $("#roll1").click(() => {
+    event.preventDefault();
+    var dice1 = parseInt(Math.floor(Math.random() * 6 + 1))
+    $(".den").text(dice1)
+
+    if (dice1 ===1 ){
+      alert(" oops!you cant roll")
+      total1 =0;
+      $(".den2").text(total1);
+    }
+    else{
+
+  total1 = total1 + dice1;
+
+      $(".den2").text(total1);
+   }
 
 });
-
 });
