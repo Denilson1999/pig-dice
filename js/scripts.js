fd29446c0beb44a2b7e84d6e1104ab9c2c21e5b1
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-04-05 16:12:37 +0300

    jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
+$(document).ready(function(){
+  // var sum=[]
+  // function denilson(){
+  //   var dice1 = parseInt(Math.floor(Math.random()*6 +1))
+  //    $(".den").text(dice1;)
+  //   sum.push(dice1)
+  //   alert(sum)
+  // }
+  //
 
+$("#roll1").click(()=>{
+  event.preventDefault();
+    var dice1 = parseInt(Math.floor(Math.random()*6 +1))
+  $(".den").text(dice1)
 
+});
 
-
-
-
-var sum=[]
-function denilson(){
-  var dice1 = parseInt(Math.floor(Math.random()*6 +1))
-  alert(dice1)
-  sum.push(dice1)
-  alert(sum)
-  sum.forEach(function(sums){i++})
-  alert(sums)
-}
+});
