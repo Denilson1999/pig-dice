46658e3ebd7f052b9a003ef3241f1d60675c197d
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-04-05 13:10:44 +0300

    initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+var sum=[]
+function denilson(){
+  var dice1 = parseInt(Math.floor(Math.random()*6 +1))
+  alert(dice1)
+  sum.push(dice1)
+  alert(sum)
+  sum.forEach(function(sums){i++})
+  alert(sums)
+}
